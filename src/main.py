5249bc7b68a106c55a2a1dbc99c675596d92a52d
Jannis Gehring <jannis-gehring@t-online.de>
2024-11-08 22:13:10 +0100

    first main.py

---
+++ src/main.py
@@ -0,0 +1,23 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.manifold import TSNE
+from sklearn.decomposition import PCA
+
+def plot(method: str, tuples):
+    plt.title(f"Dimensionality Reduction with {method}")
+    plt.rcParams['figure.dpi'] = 300
+    x, y = zip(*tuples)
+    plt.scatter(x, y)
+    plt.show()
+
+print("hello world")
+X = np.array([[-1, -1, 3, 4], [-2, -1, 3, -2], [-3, -2, 0, 1], [1, 1, -2, -4], [2, 1, 2, 1], [3, 2, 0, 4]])
+pca = PCA(2)
+tsne = TSNE(2)
+Y_pca = pca.fit_transform(X)
+Y_tsne = tsne.fit_transform(X)
+
+print(Y_pca)
+print(Y_tsne)
+plot("PCA", Y_pca)
+plot("t-SNE", Y_tsne)
