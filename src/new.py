202312ba81603392c9fadbc0163d8eede762a0f1
Jannis Gehring <jannis-gehring@t-online.de>
2024-11-12 17:21:58 +0100

    first draft of analysis

--- src/new.py
+++ src/new.py
@@ -2,12 +2,19 @@ import psycopg
 import os
 from pgvector.psycopg import register_vector
 from load import get_insert_statement
+from plot import plot
+from sklearn.manifold import TSNE
+from sklearn.decomposition import PCA
+import numpy as np
 
+# Setup database
 conn = psycopg.connect(
-    f"dbname='{os.environ["POSTGRES_DB"]}' user='{os.environ["POSTGRES_USER"]}' password='{os.environ["POSTGRES_PASSWORD"]}' host='localhost' port='5432'"
+    f"dbname='{os.environ["POSTGRES_DB"]}'\
+    user='{os.environ["POSTGRES_USER"]}'\
+    password='{os.environ["POSTGRES_PASSWORD"]}'\
+    host='localhost' port='5432'"
 )
 cur = conn.cursor()
-
 cur.execute("CREATE EXTENSION IF NOT EXISTS vector")
 register_vector(conn)
 
@@ -25,12 +32,37 @@ cur.execute(
 # Insert data to DB
 insertStatement = get_insert_statement("data/train.csv")
 cur.execute(insertStatement)
-# Get data for dimensionality reduction
-cur.execute("SELECT count(*) FROM songs")
-print("im here")
-data = cur.fetchall()
-print(data)
 
+# Get data for analysis
+selectStatement = """
+SELECT *
+FROM songs
+WHERE genre in ('world-music', 'afrobeat', 'alt-rock', 'hard-style')
+"""
+# todo: change, so that genre-selection is possible
+def getVectors(data: list) -> list:
+    vectors: list[list[float]] = []
+    for song in data:
+        vectorString: str = song[len(song)-1]
+        vectorString = vectorString[1:len(vectorString)-1]
+        stringList: list[str] = vectorString.split(",")
+        vector: list[float] = list(map(lambda string: float(string), stringList))
+        vectors.append(vector)
+    return np.array(vectors)
+
+cur.execute(selectStatement)
+data = cur.fetchall()
+vectors = getVectors(data)
+names = list(map(lambda song: song[1], data))
+artists = list(map(lambda song: song[2], data))
+print(vectors)
 # Setup PCA and t-SNE functions
-#pca = PCA(2)
-#tsne = TSNE(2)
+pca = PCA(2)
+tsne = TSNE(2)
+
+# Execute functions
+outputVectorsPCA = pca.fit_transform(vectors)
+outputVectorsTSNE = tsne.fit_transform(vectors)
+
+plot("",outputVectorsPCA)
+plot("",outputVectorsTSNE)
--- src/plot.py
+++ src/plot.py
@@ -1,9 +1,15 @@
 import numpy as np
-import matplotlib.pyplot as plt
+import plotly.express as px
+#def plot(method: str, tuples):
+#    plt.title(f"Dimensionality Reduction with {method}")
+#    plt.rcParams['figure.dpi'] = 3000
+#    x, y = zip(*tuples)
+#    plt.scatter(x, y)
+#    plt.show()
 
-def plot(method: str, tuples):
-    plt.title(f"Dimensionality Reduction with {method}")
-    plt.rcParams['figure.dpi'] = 300
-    x, y = zip(*tuples)
-    plt.scatter(x, y)
-    plt.show()
+def plot(method: str, coords: np.ndarray) -> None:
+    X = list(map(lambda coord: coord[0], coords))
+    Y = list(map(lambda coord: coord[1], coords))
+   
+    fig = px.scatter(X, Y)
+    fig.show()
